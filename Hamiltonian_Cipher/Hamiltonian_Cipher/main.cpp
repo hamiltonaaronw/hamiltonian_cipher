77f529936f5eef53c6263fee52833460ceeb091c
aaron.hamilton <aaron.hamilton@mymail.champlain.edu>
2018-05-16 20:50:56 -0400

    added some checks to make sure selection inputs are valid

--- Hamiltonian_Cipher/Hamiltonian_Cipher/main.cpp
+++ Hamiltonian_Cipher/Hamiltonian_Cipher/main.cpp
@@ -38,25 +38,49 @@ void intro()
 
 int encryptOrDecrypt()
 {
-	int sel;
-	cout << "Would you like to" << endl
-		<< "1) Encrypt" << endl
-		<< "2) Decrypt" << endl
-		<< "Selection: ";
-	cin >> sel;
+	int sel = 0;
+	while (1)
+	{
+		cout << "Would you like to" << endl
+			<< "1) Encrypt" << endl
+			<< "2) Decrypt" << endl
+			<< "Selection: ";
+		cin >> sel;
+
+		if (sel == 1 || sel == 2)
+			break;
+		else 
+			cout << "Sorry, invalid input. Try again!" << endl << endl;
+	}
 	cout << endl;
 	return sel;
 }
 
 int inputOrFile()
 {
-	int sel;
-	cout << "Would you like to" << endl
-		<< "1) Input key and text manually" << endl
-		<< "2) Read in key and text from file" << endl
-		<< "Selection: ";
-	cin >> sel;
-	cin.get();
+	int sel = 0;
+	while (1)
+	{
+		cout << "Would you like to" << endl
+			<< "1) Input key and text manually" << endl
+			<< "2) Read in key and text from file" << endl
+			<< "Selection: ";
+		cin >> sel;
+		cin.get();
+
+		if (sel == 1 || sel == 2)
+			break;
+		else
+			cout << "Sorry, invalid input. Try again!" << endl << endl;
+	}
+	
+	// TEMPORARY - will always return 1 until I get file i/o integrated
+	if (sel == 2)
+	{
+		cout << "Sorry, File I/O not in yet, enter information manually!" << endl;
+		sel = 1;
+	}
+
 	cout << endl;
 	return sel;
 }
